c2bff5d6a7352c94ac4bb02ebcd6c57fde0c9d2f
tomek <tomeksochacki@wp.pl>
2020-04-02 23:52:42 +0200

    Initialize repository

---
+++ src/com/example/constructor/Animals.java
@@ -0,0 +1,23 @@
+package com.example.constructor;
+
+public class Animals {
+    private String name;
+    private int age;
+    private String type;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+    public void setAge(int age){
+        this.age = age;
+    }
+    public int getAge(){
+        return age;
+    }
+    public void setType(String type){
+        this.type = type;
+    }
+}
---
+++ src/com/example/constructor/Main.java
@@ -0,0 +1,9 @@
+package com.example.constructor;
+
+public class Main {
+    public static void main(String[] args) {
+        Animals animals1 = new Animals();
+animals1.setAge(4);
+        System.out.println(animals1.getAge());
+    }
+}
